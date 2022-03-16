2ccc89664c36a48236dcbe2b3cbfd64a6851ca3b
ManoharTeja <83935048+ManoharTeja@users.noreply.github.com>
2022-03-16 12:06:43 +0000

    Removed unneccesary lines
    Lines written to calculate constant (C) in v^2 = C*exp(2*\sigma*t) are removed

--- growth_rate.py
+++ growth_rate.py
@@ -12,37 +12,16 @@ nx, ny = (200, 200)
 f = h5py.File('analysis_tasks2/analysis_tasks2_s1/analysis_tasks2_s1_p0.h5','r')
 
 y = f['/scales/y/1.0'][:] # y = f['/scales/y'] gives the sub-groups in group y. 
-			   # So, to print the data in y, use the syntax in line 11.
 x = f['/scales/x/1.0'][:]
 v = f['/tasks/v'][:]
 t = f['scales']['sim_time'][:]
 f.close()
 
-amp = 0.0002
 h = Lx/nx
-k = np.exp(-(y[100]*y[100])/(0.2*0.2))
-a = 0
 vv = 0
-c = [0]*len(v)
-lnc = [0]*len(v)
 vel = [0]*len(v)
-lnvel = [0]*len(v)
-sigma = [0]*len(v)
 
-for j in range(len(v)):
-	for i in range(nx):
-		if i == 0 or i == (nx-1):
-			a = a + (1-np.cos(4*np.pi*x[i]))
-			
-		elif i % 2 == 0:
-			a = a + 4*(1-np.cos(4*np.pi*x[i]))
-			
-		else:
-			a = a + 2*(1-np.cos(4*np.pi*x[i]))	
-			
-	c[j] = (h*k*amp*a)/3
-	lnc[j] = np.log(c[j])
-	
+for j in range(len(v)):	
 	for i in range(nx):
 		if i == 0 or i == (nx-1):
 			vv = vv + v[j,i,100]*v[j,i,100]
@@ -50,12 +29,8 @@ for j in range(len(v)):
 			vv = vv + 4*v[j,i,100]*v[j,i,100]
 		else:
 			vv = vv + 2*v[j,i,100]*v[j,i,100]
-		
-	vel[j] = (h/3)*vv
-	lnvel[j] = np.log(vel[j])
 
-for j in range(1, len(v)):
-	sigma[j] = (lnvel[j] - lnc[j])/(2*t[j])
+	vel[j] = (h/3)*vv
 
 # Comparison with analytical method
 t1 = np.linspace(0, 5, 50)
@@ -75,11 +50,5 @@ plt.legend(["Simulation", "Analytical with factor", "Analytical without factor"]
 plt.grid(True, which="both")
 plt.xlabel(r'$t$', fontsize=24)
 plt.ylabel(r'$v^2$', fontsize=24)
-'''
-plt.figure(figsize=(12, 6), dpi=80)
-plt.plot(t, sigma, linestyle='--', marker='o', color='b')
-plt.xlabel(r'$t$', fontsize=24)
-plt.ylabel(r'$\sigma$', fontsize=24)
-'''
-plt.show()
 
+plt.show()
